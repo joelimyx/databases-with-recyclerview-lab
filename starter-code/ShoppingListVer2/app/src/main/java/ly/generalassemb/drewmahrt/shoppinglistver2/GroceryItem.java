81ecb8c8dd99a8b42b8a868be97b0f584d2c00c1
Joe Lim <joelimyx@gmail.com>
2016-10-25 16:43:36 -0400

    Finish homework

---
+++ starter-code/ShoppingListVer2/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistver2/GroceryItem.java
@@ -0,0 +1,21 @@
+package ly.generalassemb.drewmahrt.shoppinglistver2;
+
+/**
+ * Created by Joe on 10/25/16.
+ */
+
+public class GroceryItem {
+    private String mName;
+
+    public GroceryItem(String name) {
+        mName = name;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+}
--- starter-code/ShoppingListVer2/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistver2/MainActivity.java
+++ starter-code/ShoppingListVer2/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistver2/MainActivity.java
@@ -4,23 +4,36 @@ import android.content.Context;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import ly.generalassemb.drewmahrt.shoppinglistver2.setup.DBAssetHelper;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.recycler_view) RecyclerView mRecyclerView;
+    GroceryDatabaseHelper mHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         //Ignore the two lines below, they are for setup
         DBAssetHelper dbSetup = new DBAssetHelper(MainActivity.this);
         dbSetup.getReadableDatabase();
 
+        mHelper = GroceryDatabaseHelper.getInstance(this);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(new GroceryViewAdapter(mHelper.getItemAsList()));
     }
 }
